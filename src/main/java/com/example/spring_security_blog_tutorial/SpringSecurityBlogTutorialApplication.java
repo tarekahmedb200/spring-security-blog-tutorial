e41b5e406d991bc945e291c50bfb99161c5b9ff3
Admin <admin@192.168.1.9>
2026-01-12 14:38:09 +0200

    init commit

---
+++ src/main/java/com/example/spring_security_blog_tutorial/SpringSecurityBlogTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_blog_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBlogTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBlogTutorialApplication.class, args);
+	}
+
+}
